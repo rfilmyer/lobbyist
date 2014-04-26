b66d2d394dc9c608d505413887b9d25f1838c9ba
Roger Filmyer <roger.filmyer@gmail.com>
2014-04-26 14:38:47 -0400

    Added Player.introduce(). Orgs should now be proper nouns.

--- bio.py
+++ bio.py
@@ -4,12 +4,13 @@ import random
 #consider folding these functions into classes?
 def issue():
     issues = ("Immigration", "Healthcare Reform", "Gun Control",
-    "Tax Reform")
+    "Tax Reform", "National Security")
     return random.choice(issues)
 
 def org():
-    prefix = ("Americans for {0}", "American Forum for {0}",
-    "National {0} Foundation", "{0} Association of America")
+    prefix = ("American {0} Committee", "American Forum for {0}",
+    "National {0} Foundation", "{0} Association of America",
+    "Foundation for Responsible {0}")
     return random.choice(prefix)
 
 def partisan():
@@ -24,6 +25,14 @@ class Player(object):
         self.issue = issue()
         self.org_raw = org()
         self.org = self.org_raw.format(self.issue)
+    def introduce(self):
+        '''Gives the player the background story'''  
+        print 'The year is 2014, and the hot issue of the day is ' \
+        '{0}. The media has been playing up a policy battle between ' \
+        'Democrats and Republicans.'.format(self.issue)
+        print 'As a fellow at the {0}, you are one of the forefront '\
+        'experts on this issue.'.format(self.org)
+        print "Congress has called you to testify at its hearing next week."
 
 class Rep(object):
-    
+    pass 
