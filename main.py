1fe9f7267af062e22ae7edada5d9cf5bd053af5b
Roger Filmyer <roger.filmyer@gmail.com>
2014-05-08 13:19:37 -0400

    Added a quick test suite

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import hearing
 player = bio.Player()
 player.create()
 player.introduce()
+
+issue = bio.issue()
+
+firstrep = bio.Rep('Mitt','Kerry')
+first_q = hearing.Question()
+first_q.ask(issue,firstrep)
