dcc477fc54385ad1b60e80b32cd9e779b6437a46
Roger Filmyer <roger.filmyer@gmail.com>
2014-05-08 18:33:36 -0400

    Added introduction, questions now actually asked.
    I added a real introduction to the game, which is in main.py
    Also added a question-asking and answering mechanism. There's
    logic to deal with answering questions
    At this point, however, only the first rep asks a question.
    My first thought is to make it a for-loop, but I'm not sure if
    I'm going to do that before or after I make actual answers to the questions.

--- hearing.py
+++ hearing.py
@@ -6,9 +6,30 @@ class Question(object):
         'kids' : "What about the children? What impact does {0} "
         "legislation have on my younger constituents?",
         'deficit' : "What impact does {0} have on the deficit?",
+        'elec_year' : "This is an election year. Why take a risk now?"
         }
         self.q = random.choice(self.question_pool.keys())
 
-    def ask(self,issue,asker):
-        print("Representative " + asker.lname + " asks:")
+    def ask(self,issue,rep):
+        print("Representative " + rep.lname + " asks:")
         print(self.question_pool.get(self.q).format(issue))
+        answer = ''
+        while answer != '1' and answer != '2':
+             print('You may:')
+             print('1) Minimize the issue')
+             print('2) Play up the issue')
+             answer = raw_input('> ')
+        play_up = bool(int(answer) - 1)
+        self.answer(issue,rep,play_up)
+
+    def answer(self,issue,rep,play_up):
+        if rep.in_favor:
+            if play_up:
+                print('you chose wisely')
+            else:
+                print('You fool of a took!')
+        else:
+            if play_up:
+                print('Well great, now the rep is even more worried!')
+            else:
+                print('No worries m8')
--- main.py
+++ main.py
@@ -3,10 +3,43 @@ import hearing
 
 player = bio.Player()
 player.create()
-player.introduce()
 
-issue = bio.issue()
-
+#later in life I'll use random names from the "names" module...
 firstrep = bio.Rep('Mitt','Kerry')
+secondrep = bio.Rep('Nancy','Boehner')
+thirdrep = bio.Rep('George','Gore')
+fourthrep = bio.Rep('Harry','McConnell')
+fifthrep = bio.Rep('Rand','Warren')
+reps = [firstrep, secondrep, thirdrep, fourthrep, fifthrep]
+
+player.introduce()
+raw_input()
+#put the bios here
+print('You will testify before a panel of five members of '
+'congress. Your goal is to sway the panel towards your viewpoint '
+'on {0}.'.format(player.issue))
+print('Each member has a different opinion, so tailor your answers to'
+' each representative.')
+raw_input()
+
+print('The representatives are:')
+for rep in reps:
+    if rep.in_favor:
+        opinion = 'Favorable'
+    else:
+        opinion = 'Opposed'
+    print(rep.fname + ' ' + rep.lname + ': ' + opinion)
+raw_input()
+
+print('HEARING DAY')
+print('-----------')
+print('After prepping all night, you walk up the steps to the Capitol,'
+'entering the House chambers, notes in hand. Within a few minutes '
+'(okay, let\'s be realistic, more like thirty), the members of '
+'congress enter and take their seats. The meeting is ready to begin.')
+raw_input()
+
+
+
 first_q = hearing.Question()
-first_q.ask(issue,firstrep)
+first_q.ask(player.issue,firstrep)
